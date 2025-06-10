b3f5d29766fa8b7e0a2f99af3e81e156c34d522b
Denils <denilson78718599@gmail.com>
2025-06-10 03:23:43 +0200

    Esto seria mi tarea

---
+++ Tarea/flutter_application_2/lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(denil());
+}
+class denil extends StatelessWidget{
+  @override
+  Widget build(BuildContext Context){
+    return MaterialApp(
+      home: Scaffold(
+        body: Center(
+          child: Text("HOLA MUNDO O IMBOLJAP"),
+        ),
+      ),
+    );
+  }
+}
+
